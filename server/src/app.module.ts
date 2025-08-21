4291f417b01c3eb147a6ced83c397c669d82d27f
최현준 <choijjunii1@gmail.com>
2025-08-21 17:20:31 +0900

    nest json 저장 방식

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { SaveModule } from './save/save.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,  // 전역으로 사용 가능
     }),
+    SaveModule,
   ],
   controllers: [AppController],
   providers: [AppService],
