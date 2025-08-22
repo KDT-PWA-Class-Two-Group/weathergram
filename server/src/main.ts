1ad04de2fa987d522245bff478fe673dc462e385
최현준 <choijjunii1@gmail.com>
2025-08-22 11:22:56 +0900

    feat: swagger UI 생성

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import helmet from 'helmet'
 import { ConfigService } from '@nestjs/config'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -16,8 +18,28 @@ async function bootstrap() {
     credentials: true,
   })
 
-  // 공통 prefix -> /api/...
-  // app.setGlobalPrefix('api')
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 필드 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 필드가 있으면 에러 발생
+      transform: true, // 요청 데이터를 DTO로 변환
+    }),
+  )
+
+  // Swagger 설정
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Weathergram API')
+    .setDescription('Weathergram API 문서')
+    .setVersion('1.0.0')
+    .addBearerAuth() // JWT 인증 추가
+    .build()
+  
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('/', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // 인증 정보 유지
+    },
+  })
 
   // 포트도 .env에서 불러오기
   const PORT = configService.get<number>('PORT') || 8080
