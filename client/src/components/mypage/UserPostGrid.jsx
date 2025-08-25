7e9e7fb9a4b75b05afc72fd457ce30113899769c
김혜민 <g955m22@gmail.com>
2025-08-25 18:13:26 +0900

    업로드 게시글 갤러리 경로 작성

--- client/src/components/mypage/UserPostGrid.jsx
+++ client/src/components/mypage/UserPostGrid.jsx
@@ -1,16 +1,30 @@
+import React, {useState, useEffect} from 'react'
+
 export default function UserPostGrid(){
+    const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    //사용자 업로드 게시글 api호출
+    fetch('경로')
+    .then(response => response.json())
+    .catch(error => console.error('호출 오류', error))
+
+    setPosts()
+  }, [])
+
   return(
-    <div className='w-100 h-auto marginTop-70px'
-          style={{
-            backgroundColor: "#eee",
-          }}>
-            <div className="이미지(임시)"
+    <div className='w-100 h-auto marginTop-70px' style={{backgroundColor: "#eee"}}>
+      {posts.map(post => (
+        <div key={post.id}>
+          <img src={post.imgUrl} alt='사용자 게시물 이미지'
               style={{
                 width:120,
                 height:120,
-                backgroundColor: "salmon"
-              }}>
-            </div>
-          </div>
+                objectFit: 'cover'
+              }}
+            />
+        </div>
+      ))}        
+      </div>
   )
 }
