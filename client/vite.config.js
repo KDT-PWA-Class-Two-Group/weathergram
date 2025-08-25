ced3656a6b232465d6cd21a6b6732aa19073f544
csm <csm009177@gmail.com>
2025-08-25 14:57:50 +0900

    vite.config.js - target 8008으로 변경하고 빌드하여 dist 생성

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // Nest 포트
+        target: 'http://localhost:8080', // Nest 포트
         changeOrigin: true
       }
     }
